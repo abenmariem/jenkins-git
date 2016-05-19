79a49c92ab5db8e8fec8ad599dfed35f60350cba
abenmariem <abderrahmen.ben.mariem@gmail.com>
2016-05-19 11:37:38 +0100

    Fixing test configuration and removing the unnessary helpers folder

--- person-webapi-v1/src/test/java/personsManagement/PersonWebapiV1ApplicationTests.java
+++ person-webapi-v1/src/test/java/personsManagement/PersonWebapiV1ApplicationTests.java
@@ -7,9 +7,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import personsManagement.web.boot.PersonWebapiV1Application;
+import personsManagement.web.config.AppConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = PersonWebapiV1Application.class)
+@SpringApplicationConfiguration(classes = AppConfig.class)
 @WebAppConfiguration
 public class PersonWebapiV1ApplicationTests {
 
